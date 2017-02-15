e00d4bddcbf0427bf4ed0343c41862939516176c
Trygve <tnordberg@smm.org>
2017-02-15 16:42:41 -0600

    Home button

--- imports/ui/components/VideoHomeButton.js
+++ imports/ui/components/VideoHomeButton.js
@@ -5,7 +5,7 @@ class VideoHomeButton extends React.Component {
   render() {
     return (
       <div onClick={this.props.homeAction.bind(this)} className="home-button">
-        <img src="/images/home.png" />
+        <img src="/images/homeIcon.png" />
       </div>
     );
   }
